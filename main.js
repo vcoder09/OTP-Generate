bb4ec9d1b8528b0d3baa04a94933986160c75f2c
Akkii <vaibhavsanas1703@gmail.com>
2024-06-08 11:44:43 +0530

    update

---
+++ main.js
@@ -0,0 +1,33 @@
+let value = Math.floor(1000 + Math.random() * 9000)
+
+let enterOtp = document.getElementById("input");
+
+let resendOTP = document.getElementById("ref");
+
+let displayOtp = document.getElementById("otp");
+
+// displayOtp.innerHTML = "Your OTP is: " + value;
+
+function verifyOTP(){
+ 
+  let uservalue = enterOtp.value;
+
+  if(uservalue == ""){
+    alert("Please Enter OTP")
+  }
+  else if (uservalue.length > 6) {
+    alert("OTP is 4 digits Only")
+  }
+  else if(value != uservalue){
+    alert("OTP is Incorrect")
+  }
+  else{
+    alert("OTP Verify Successfully")
+    location.href = "https://codervv.netlify.app"
+  }
+}
+
+resendOTP.addEventListener("click",() => {
+  alert(`Your OTP is: ${value}`)
+});
+
